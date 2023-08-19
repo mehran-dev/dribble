c7a4f2613970fe1b7abb80057f359d3ce5788a10
mehran <m.saghebi.developer@gmail.com>
2023-08-19 17:44:31 +0330

    added sizes

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['lh3.googleusercontent.com', 'res.cloudinary.com', 'task.com']
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    domains: ["lh3.googleusercontent.com", "res.cloudinary.com", "task.com"],
   },
   experimental: {
-    serverComponentsExternalPackages: ['cloudinary', 'graphql-request']
+    serverComponentsExternalPackages: ["cloudinary", "graphql-request"],
   },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
-    ignoreDuringBuilds: true
-  }
-}
+    ignoreDuringBuilds: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
